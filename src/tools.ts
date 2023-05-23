a5904fa98eb0895c9d8c226834ff6c7a42f196a5
Ivan Monastyrev <ikloster@yandex.ru>
2023-05-24 01:15:10 +0300

    fix(tools):  getFileExtension

--- src/tools.ts
+++ src/tools.ts
@@ -1,6 +1,12 @@
+import { extname } from 'path';
 import { Filepath } from './types';
+import { EMPTY } from './const';
 
-export const getFileExtension = (filePath: Filepath): string => filePath.split('.').pop();
+export const getFileExtension = (filePath: Filepath): string => {
+  const fileExtension = extname(filePath);
+
+  return fileExtension.length > 0 ? fileExtension.slice(1) : EMPTY;
+};
 
 export const hasJsExtension = (filePath: Filepath): boolean => {
   const JS_EXTENSIONS = ['js', 'ts', 'cjs', 'mjs'];
