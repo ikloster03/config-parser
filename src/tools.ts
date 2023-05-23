81df31d2e536c09213101a4a9479a10bb5d99c64
Ivan Monastyrev <ikloster@yandex.ru>
2023-05-24 00:30:39 +0300

    refactor(tools): extract getFileExtension

--- src/tools.ts
+++ src/tools.ts
@@ -1,9 +1,11 @@
 import { Filepath } from './types';
 
+export const getFileExtension = (filePath: Filepath): string => filePath.split('.').pop();
+
 export const hasJsExtension = (filePath: Filepath): boolean => {
   const JS_EXTENSIONS = ['js', 'ts', 'cjs', 'mjs'];
 
-  const fileExtension = filePath.split('.').pop();
+  const fileExtension = getFileExtension(filePath);
 
   return JS_EXTENSIONS.includes(fileExtension);
 };
