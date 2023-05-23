49efd9c83ffc8e19ba1fe9f8bf89365e8d7c1591
Ivan Monastyrev <ikloster@yandex.ru>
2023-05-24 00:31:53 +0300

    feat: update index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-export const hello = () => 'hello';
+export * from './extension-provider.interface';
+export * from './config-parser';
+export { default as RcParser } from './rc-parser';
 
-export default hello;
+export { default as JsExtensionProvider } from './providers/js-extension-provider';
+export { default as JsonExtensionProvider } from './providers/json-extension-provider';
+export { default as YamlExtensionProvider } from './providers/yaml-extension-provider';
